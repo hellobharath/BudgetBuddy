d7d632f0da05713b53a6b65b3faf5941978a945c
Bharath Shenoy <bharathshn331@gmail.com>
2024-12-07 17:56:48 +0530

    Shree Ganesh(Initial) commit

---
+++ src/test/java/com/rupee/BudgetBuddy/BudgetBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupee.BudgetBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
