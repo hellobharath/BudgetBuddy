db037989cb21736bc0abc77bfdae52e720c5062f
Bharath Shenoy <bharathshenoy.1rn17cs026@gmail.com>
2025-10-26 22:34:12 +0530

    Add Devtools, User entity and hibernate auto update

---
+++ UserService/README.md
@@ -0,0 +1,3 @@
+# Budget Buddy - User Service
+
+This service primarily deals with User details management, authenticating user using JWT authentication
---
+++ UserService/src/main/java/com/example/UserService/Entity/UserEntity.java
@@ -0,0 +1,19 @@
+package com.example.UserService.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    private String name;
+
+    private int Age;
+
+}
